2666b17c29a2b7a757702bf71e87d33d1b6c304f
IggyKing <1278532727@qq.com>
2026-04-19 14:59:48 +0800

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,11 @@
+#total = 0
+#for a in range(1,101):
+ #   print(a)
+ #   total = total + a
+#print("1到100的和为：" ,total)
+
+b = input("请输入：")
+if(b % 2 ==0):
+    print(f"{num}是偶数")
+else:
+    print(f"{num}是奇数")
---
+++ Day1_p.py
@@ -0,0 +1,28 @@
+#(1)输入输出
+print("Hello")
+a = input("请输入：")
+print(a)
+
+#(2)变量
+a = 10
+b = 3.14
+s = "Hello"
+
+#(3)if判断（重点：缩进）
+a = 10
+b = 5
+if a > b :
+    print(a)
+else :
+    print(b)
+
+#(4)for循环
+for i in range(5):
+    print(i)
+
+#(5)while循环
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+

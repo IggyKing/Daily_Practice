bd480a5d5ad1e9694ba4ad444bc9d0af65f6eb5e
IggyKing <1278532727@qq.com>
2026-05-09 15:57:07 +0800

    Add files via upload

--- day07/Day7.py
+++ day07/Day7.py
@@ -18,3 +18,34 @@ if len(stack) == 0:
     print(True)
 else:
     print(False)
+
+#完整逻辑：
+s = "()[]{}"
+
+stack = []
+valid = True #合不合法的标记 一开始默认这个括号字符串是合法的
+
+for c in s:
+    if c == "(" or c == "[" or c == "{":
+        stack.append(c)
+    else:
+        if len(stack) == 0:
+            valid = False
+            break
+
+        top = stack[-1]
+
+        if c == ")" and top == "(":
+            stack.pop()
+        elif c == "]" and top == "[":
+            stack.pop()
+        elif c == "}" and top == "{":
+            stack.pop()
+        else:
+            valid = False
+            break
+
+if len(stack) == 0 and valid:
+    print(True)
+else:
+    print(False)
